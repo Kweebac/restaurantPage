5cefb5b571eddc64f896c3d462283ed43f6c16a2
Kweebac <06.amarshall@gmail.com>
2023-05-02 17:25:18 +0100

    Added functionality to the 2 buttons in the middle on the home page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import { page } from "./page-load";
 
+function middleHomeButtons() {
+  const middleMenuButton = document.querySelector(
+    ".middle button:first-child"
+  );
+  const middleContactButton = document.querySelector(
+    ".middle button:last-child"
+  );
+  middleMenuButton.addEventListener("click", () => {
+    page.remove();
+    page.create.menu();
+  });
+  middleContactButton.addEventListener("click", () => {
+    page.remove();
+    page.create.contact();
+  });
+}
+
+// Only used once
 page.build();
+middleHomeButtons();
 
 const homeButton = document.querySelector(".list > div:first-child");
 const menuButton = document.querySelector(".list > div:nth-child(2)");
@@ -9,6 +28,8 @@ const contactButton = document.querySelector(".list > div:last-child");
 homeButton.addEventListener("click", () => {
   page.remove();
   page.create.home();
+
+  middleHomeButtons();
 });
 
 menuButton.addEventListener("click", () => {
