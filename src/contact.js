b2bdf05867d2e4f9f058846a98a2fe7899844dbc
Kweebac <06.amarshall@gmail.com>
2023-05-02 02:38:19 +0100

    Added createContact()

---
+++ src/contact.js
@@ -0,0 +1,60 @@
+const contentDiv = document.querySelector("#content");
+
+function createContact() {
+  contentDiv.innerHTML = `
+  <div class="header">
+    <div>
+      <span>Breanna's</span>
+      <span>Cooking</span>
+    </div>
+    <div class="list">
+      <div>HOME</div>
+      <div>OUR MENU</div>
+      <div>CONTACT</div>
+    </div>
+  </div>
+  <div class="form">
+    <div class="title">
+      <div>
+        <span>Breanna's</span>
+        <span>Cooking</span>
+      </div>
+      <div>
+        14029 Lind Village, Apt. 219, SC5 9ZH, Port Cayla, Hawaii, Great
+        Britain
+      </div>
+      <div>
+        <div>+44 7704 607373</div>
+        <div>+44 7193 889240</div>
+      </div>
+    </div>
+    <div>
+      <form action="" method="post">
+        <div>
+          <label for="name">Name:</label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div>
+          <label for="email">Email:</label>
+          <input type="email" name="email" id="email" />
+        </div>
+        <div>
+          <label for="message">Message:</label>
+          <textarea
+            name="message"
+            id="message"
+            cols="50"
+            rows="10"
+          ></textarea>
+        </div>
+        <button>Submit</button>
+      </form>
+      <img
+        src="https://nyf005.github.io/restaurant-project/3b392d7cb004d88af9dd.png"
+        alt=""
+      />
+    </div>
+  </div>`;
+}
+
+export { createContact };
